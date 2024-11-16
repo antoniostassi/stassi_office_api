6e62f1edde72506bd745961d1923e3c0d394df52
Antonio Stassi <stassiant@outlook.it>
2024-11-17 00:09:30 +0100

    requesting authentication for invoices apis

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('invoices', InvoiceController::class)->only([
+Route::resource('invoices', InvoiceController::class)->middleware('auth')->only([
     'index',
     'show'
 ]);
