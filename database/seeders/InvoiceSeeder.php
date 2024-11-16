9695c75a016a8fd21463eb9c394de7d5c79b6ff5
Antonio Stassi <stassiant@outlook.it>
2024-11-16 20:57:51 +0100

    Invoice Seeding for testing purposes

--- database/seeders/InvoiceSeeder.php
+++ database/seeders/InvoiceSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class InvoiceSeeder extends Seeder
 {
@@ -13,5 +16,16 @@ class InvoiceSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('invoices')->truncate();
+        for ($i=0; $i < 15; $i++) { 
+            # code...
+            DB::table('invoices')->insert([
+                'description' => Str::random(10),
+                'amount' => rand(100, 1500),
+                'received' => rand(0,1),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]);
+        }
+        
     }
 }
