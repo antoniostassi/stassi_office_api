e1586a7e1387bbda2a974b2bcf252c554566a420
Antonio Stassi <stassiant@outlook.it>
2024-11-16 20:58:08 +0100

    invoices api test route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Models
+
+use App\Models\Outgoing;
+use App\Models\Invoice;
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/invoices', function() {
+    $invoices = Invoice::all();
+    return $invoices;
 });
