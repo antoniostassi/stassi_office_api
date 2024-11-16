73cbbae1f43c66c04954b38bdd173289c64d88bc
Antonio Stassi <stassiant@outlook.it>
2024-11-16 20:33:56 +0100

    Update route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Stassi\'s Office Back-end Status' => 'Online','Laravel Version' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
